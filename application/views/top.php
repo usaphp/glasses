af15302ffce3ecde026296b0276335cdf8ef54fb
Desktop windows <usaphp@gmail.com>
2010-12-23 11:13:02 +0300

    added iamges

---
+++ application/views/top.php
@@ -0,0 +1,12 @@
+<div>
+	<a href='#' title='migaz.com' class='span-6'><img src='/images/layout/logo.png' alt='Migaz.com'/></a>
+	<div class='span-10'>
+		<form>
+			<label>Search</label>
+			<input type='text'/>
+			<button>Search</button>
+		</form>
+	</div>
+	<div class='span-8'></div>
+	<div class='clear'></div>
+</div>
